28c0cc37e58733a954c4d9d2ba53d0881103c57f
Bradley Eaton <bradley@bradleyeaton.com>
2025-06-26 11:13:32 +0100

    Fix WebView2 focus without hack.

--- Jellyfin/Controls/JellyfinWebView.xaml.cs
+++ Jellyfin/Controls/JellyfinWebView.xaml.cs
@@ -15,16 +15,17 @@ namespace Jellyfin.Controls
     public sealed partial class JellyfinWebView : UserControl, IDisposable
     {
         private readonly GamepadManager _gamepadManager;
+        private readonly WebView2 WView;
 
         public JellyfinWebView()
         {
             this.InitializeComponent();
 
+            WView = new WebView2();
             // Set WebView source
             WView.Source = new Uri(Central.Settings.JellyfinServer);
 
             WView.CoreWebView2Initialized += WView_CoreWebView2Initialized;
-            WView.NavigationStarting += WView_NavigationStarting;
             WView.NavigationCompleted += JellyfinWebView_NavigationCompleted;
             SystemNavigationManager.GetForCurrentView().BackRequested += Back_BackRequested;
 
@@ -32,15 +33,12 @@ namespace Jellyfin.Controls
             _gamepadManager = new GamepadManager();
         }
 
-        private void WView_NavigationStarting(WebView2 sender, CoreWebView2NavigationStartingEventArgs args)
-        {
-            // Workaround to fix focus issues with gamepad not focusing window
-            BtnFocusStealer.Visibility = Visibility.Visible;
-            BtnFocusStealer.Focus(FocusState.Programmatic);
-        }
-
         private void WView_CoreWebView2Initialized(WebView2 sender, CoreWebView2InitializedEventArgs args)
         {
+            // Must wait for CoreWebView2 to be initialized or the WebView2 would be unfocusable.
+            this.Content = WView;
+            WView.Focus(FocusState.Programmatic);
+
             // Set useragent to Xbox and WebView2 since WebView2 only sets these in Sec-CA-UA, which isn't available over HTTP.
             WView.CoreWebView2.Settings.UserAgent += " WebView2 " + Utils.AppUtils.GetDeviceFormFactorType().ToString();
             WView.CoreWebView2.ContainsFullScreenElementChanged += JellyfinWebView_ContainsFullScreenElementChanged;
@@ -63,11 +61,6 @@ namespace Jellyfin.Controls
                 MessageDialog md = new MessageDialog($"Navigation failed: {errorStatus}");
                 await md.ShowAsync();
             }
-
-            // Refocus WebView after having BtnFocusStealer steal it.
-            // To work around WebView2 not focusing web content.
-            WView.Focus(FocusState.Programmatic);
-            BtnFocusStealer.Visibility = Visibility.Collapsed;
         }
 
         private void JellyfinWebView_ContainsFullScreenElementChanged(CoreWebView2 sender, object args)
